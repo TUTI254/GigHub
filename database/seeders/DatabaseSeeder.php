9ae4ad6ee96bda41bc24b3c854ce06ff13e948cf
TUTI254 <collotuti6@gmail.com>
2023-06-26 18:38:04 +0300

    chore(project setup & learning basics)
    what did i do
    * Setting Up Laravel project
    * learn on Routing & Responses
    * learn on Wildcard Endpoints
    * learn on  Route Constraints
    * Get to know what Die Dump Setting Up Laravel
    * learn on Request & Query Params
    * learn on API Routes
    * View Basics & Passing Data
    * Touched on Blade Templates & Basic Directives
    * Creating a Basic Model
    * Database Setup & Config
    * Create Database & User
    * Creating Database Migrations
    * Running Migrations
    * Database Seeding
    * Create an Eloquent Model
    * Creating a Factory

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // create 10 users using the user factory
+        \App\Models\User::factory(5)->create();
+
+        // create  listings using the listing factory
+        Listing::factory(8)->create();
+        // Listing::create([
+        //     'title' => 'Need of a PHP Developer ',
+        //     'tags' => 'php,html,css',
+        //     'company' => 'Jumia',
+        //     'location' => 'Lagos, Nigeria',
+        //     'email' => 'careers@jumia.com',
+        //     'website' => 'https://jumia.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Need of a Next Developer',
+        //     'tags' => 'Next, React, Node, REST, GraphQL, TypeScript, TailwindCSS',
+        //     'company' => 'Netflix',
+        //     'location' => 'California, USA',
+        //     'email' => 'carrers@neflix.com',
+        //     'website' => 'https://netflix.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        // ]);
+
+    }
+}
